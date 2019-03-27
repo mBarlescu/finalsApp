8cfa76fe0d568e2a1a68d3c0cb001c24a588a6d7
MarkBarlescu <barlescu92@gmail.com>
2019-03-27 12:49:15 -0700

    added a search engine

--- src/components/mentors/Subjects.jsx
+++ src/components/mentors/Subjects.jsx
@@ -5,12 +5,13 @@ export default class Subjects extends Component {
 	constructor(props) {
 	    super(props);
 	    this.state = {
-	      subjects: ['psychology', 'science']
-      	
+	      subjects: ['psychology', 'science'],
+	      search: '',
     	}
     	this.subjects = ['computer', 'chem', 'history']
     	console.log(this.state.subjects)
     	this.remove = this.remove.bind(this);
+    	this.list = ['computer science', 'history', 'poker']
     }
   listofUsers(){
   	const subjects = this.state.subjects;
@@ -36,18 +37,38 @@ export default class Subjects extends Component {
   		console.log('yoolo', this.state.subjects)
   	})
 
-		}
+	}
+
+	updateSearch(event) {
+			this.setState({search: event.target.value.substr(0,20)});
+	}
+
+
 
 
 
 
 	render() {
+		let filtered = this.list.filter(
+			(list) => {
+				return list.indexOf(this.state.search) !== -1;
+			})
 		return (
 			<div>
 				{this.listofUsers()}
-				<br>
+				<br/>
 				<div>
-
+					<h3>Add</h3>
+					<input type='text'
+						value={this.state.search}
+						onChange={this.updateSearch.bind(this)} />
+				</div>
+				<div>
+					<ul>
+						{filtered.map((subject) =>{
+							return <li>{subject}</li>
+						})}
+					</ul>
 				</div>
 			</div>
 		);
